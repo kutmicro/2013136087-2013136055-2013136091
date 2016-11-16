fecce0b5ae4b001472e7e80772bdf06462e93ac5
Sunghyun Lee <sungdl93@gmail.com>
2016-11-16 09:15:42 +0900

    Modify TFT Code

--- Src/TFT/TFT.ino
+++ Src/TFT/TFT.ino
@@ -1,40 +1,63 @@
-#include <TFT.h> // Hardware-specific library
+#include <TFT.h>  // Arduino LCD library
 #include <SPI.h>
- 
-#define CS   10
-#define DC   9
-#define RESET  8  
- 
+
+// pin definition for the Uno
+#define cs   10
+#define dc   9
+#define rst  8
+
 // pin definition for the Leonardo
-// #define CS   7
-// #define DC   0
-// #define RESET  1 
- 
-TFT myScreen = TFT(CS, DC, RESET);
- 
-void setup(){
-  myScreen.begin();  
-  myScreen.background(0,0,0);  // clear the screen with black
-  delay(1000);  // pause for dramatic effect
-}
- 
-void loop(){
-  myScreen.stroke(255, 0, 0); // set the stroke color to red
-  myScreen.line(0, 10, myScreen.width(), 10); // draw a line across the screen
-  delay(1000);
- 
-  myScreen.noStroke(); // don't draw a line around the next rectangle
-  myScreen.fill(0,255,0); // set the fill color to green
-  myScreen.rect(0,20,myScreen.width(),10); //draw a rectangle across the screen
-  delay(1000);
- 
-  myScreen.fill(0,0,255); // set the fill color to blue
-  myScreen.stroke(255,255,255); // outline the rectangle with a white line
-  myScreen.rect(0,45,myScreen.width(),45); // draw a fat rectangle
-  delay(1000);
- 
-  myScreen.background(0,0,0); // clear the screen before starting again
-  delay(1000); 
+// #define cs   7
+// #define dc   0
+// #define rst  1
+
+// create an instance of the library
+TFT TFTscreen = TFT(cs, dc, rst);
+
+// char array to print to the screen
+char printMoney[6];
+
+void setup() {
+
+  // Put this line at the beginning of every sketch that uses the GLCD:
+  TFTscreen.begin();
+
+  TFTscreen.background(0, 0, 0);    // 배경 검정색으로 설정
+
 }
 
- 
+void loop() {
+  int savings = 0;
+  money(savings);
+
+//  erase the text 
+//  TFTscreen.stroke(0, 0, 0);
+//  TFTscreen.text(sensorPrintout, 0, 20);
+}
+
+
+
+
+
+
+void money(int savings) {       // 현재 저장 금액 출력 함수
+  
+  String value = String(savings);
+
+  value.toCharArray(printMoney, 6);
+
+  TFTscreen.stroke(255, 255, 255);    // 폰트 색상 설정 (흰색)
+  TFTscreen.setTextSize(2);           // 텍스트 사이즈 설정
+  TFTscreen.text("Current Savings \n ", 0, 0);        // text(text, x좌표, y좌표) 
+  TFTscreen.setTextSize(5);           // 텍스트 사이즈 설정
+  TFTscreen.text(printMoney, 0, 20);  // 출력
+  delay(250);
+
+}
+
+
+
+
+
+
+
