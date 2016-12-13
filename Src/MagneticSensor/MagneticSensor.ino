985ed868263bf4c9957ab9c72a4ceeb4fd6e721f
Sunghyun Lee <sungdl93@gmail.com>
2016-12-13 16:11:52 +0900

    Update

--- Src/MagneticSensor/MagneticSensor.ino
+++ Src/MagneticSensor/MagneticSensor.ino
@@ -8,8 +8,8 @@ void setup() {
 }
 
 void loop() {
-  //8번 핀에서 데이터를 읽어들임
-  val = digitalRead(8);
+  //2번 핀에서 데이터를 읽어들임
+  val = digitalRead(2);
   // 시리얼 모니터로 데이터 출력
   Serial.println(val);
   delay(100);
--- Src/Servo-Keypad/Servo-Keypad.ino
+++ Src/Servo-Keypad/Servo-Keypad.ino
@@ -1,30 +1,32 @@
 #include <Keypad.h>
 #include <Servo.h> //s
 #include "U8glib.h"
+#include <EEPROM.h>
  
 U8GLIB_NHD_C12864 u8g(13, 11, 10, 9, 8);    // SPI Com: SCK = 13, MOSI = 11, CS = 10, CD = 9, RST = 8
 
 Servo s; //s
-//int servoPin = 11; //s
-int min_value = 544; //s
+//int servoPin = 11;  //s
+int min_value = 544;  //s
 int max_value = 2400; //s
-int saving = 0;   // 저금 금액
-int goal = 30000;  // 목표 금액
-int sensor0 = A8;  // 아날로그의 0번 핀 연결  
-int sensor1 = A9;  // 아날로그의 0번 핀 연결
-int sensor2 = A10;  // 아날로그의 0번 핀 연결
-int sensor3 = A11;  // 아날로그의 0번 핀 연결
+int saving = 0;       // 저금 금액
+int goal = 10000;         // 목표 금액
+int set_up = 0;       // 패스워드와 목표금액이 설정되었는지 확인하는 함수
+int sensor0 = A8;     // 아날로그의 0번 핀 연결  500
+int sensor1 = A15;    // 아날로그의 0번 핀 연결  100
+int sensor2 = A10;    // 아날로그의 0번 핀 연결  50
+int sensor3 = A11;    // 아날로그의 0번 핀 연결  10
+int ch = 0;
 
 int speakerpin = 34; //스피커가 연결된 디지털핀 설정
 int note[] = {2093,2349,2637,2793,3136,3520,3951,4186}; //도레미파솔라시도
 
 char Password[4] = {'0', '0', '0', '0'};  // 설정된 패스워드 저장 배열 
 char input[4];  // 입력된 패스워드 저장 배열
+int goals[5];   // 입력되는 목표금액 저장 배열
 char key;   // 입력되는 키값 임시저장 변수                                    
-//int position = 0;
 int check = 0;   // 패스워드가 맞는지 체크하는 변수 
 int ServoSignal = 0;
-//int pos = 0;
 
 const byte numROWS = 4; // 행
 const byte numCOLS = 3; // 열
@@ -45,78 +47,144 @@ Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numROWS, numCOLS);
 void setup() 
 {
   Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
   u8g.setFont(u8g_font_unifont);
   u8g.setColorIndex(1);
   s.attach(12);
-  s.write(180);
   pinMode(10,INPUT);
+  pinMode(2, INPUT_PULLUP);
 }
 
 void loop() 
 {
-  u8g.firstPage();
-  do {
-    drawMain();
-  } while(u8g.nextPage());
-  delay(10);
-
-  int reading0 = analogRead(sensor0);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
-  float sensorV0 = reading0 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
-  int sensormV0 = sensorV0 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
-
-  int reading1 = analogRead(sensor1);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
-  float sensorV1 = reading1 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
-  int sensormV1 = sensorV1 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
-
-  int reading2 = analogRead(sensor2);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
-  float sensorV2 = reading2 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
-  int sensormV2 = sensorV2 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
-
-  int reading3 = analogRead(sensor3);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
-  float sensorV3 = reading3 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
-  int sensormV3 = sensorV3 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
-
-  if( sensormV0 <= 1000) {
-    insert500();
-    delay(1000);
-  }
-
-  else if( sensormV1 <= 1000) {
-    insert100();
-    delay(1000);
-  }
-
-  else if( sensormV2 <= 1000) {
-    insert50();
-    delay(1000);
-  }
-
-  else if( sensormV3 <= 1000) {
-    insert10();
-    delay(1000);
+  
+  while(set_up == 0) 
+  {
+    s.write(90);
+    if(ch == 0)
+    {
+      u8g.firstPage();
+      do {
+        drawSetP();
+      } while(u8g.nextPage());
+      password_key();
+      ch = 1;
+    }
+    else
+    {
+      if(digitalRead(2)) 
+      {
+        delay(1500);
+        s.write(180);
+        set_up = 1;
+        ch = 0;
+      }
+    }
   }
   
-  
-  key = myKeypad.getKey();
-  if(key == '*') {
+  while(set_up == 1)
+  {
+    
     u8g.firstPage();
     do {
-      drawAstro0();
+      drawMain();
     } while(u8g.nextPage());
     delay(10);
-    password_key();
-    input_key();
-    check_key();
+
+    int reading0 = analogRead(sensor3);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
+    float sensorV0 = reading0 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
+    int sensormV0 = sensorV0 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
+
+    int reading1 = analogRead(sensor2);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
+    float sensorV1 = reading1 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
+    int sensormV1 = sensorV1 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
+
+    int reading2 = analogRead(sensor1);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
+    float sensorV2 = reading2 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
+    int sensormV2 = sensorV2 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
+
+    int reading3 = analogRead(sensor0);   // 아날로그 0번 핀에서 0~5V 사이로 변하는 값을 0~1023 사이의 값으로 읽는다
+    float sensorV3 = reading3 * 5.0 / 1024.0; // 받아들인 값을 전압값(V)으로 변환 시킨다
+    int sensormV3 = sensorV3 * 1000;          // 전압값을 단위를 mV로 변환시킨다.
+
+    if( sensormV0 <= 1000) {
+      insert500();
+      delay(1000);
+    }
+
+    else if( sensormV1 <= 1000) {
+      insert100();
+      delay(1000);
+    }
+
+    else if( sensormV2 <= 1000) {
+      insert50();
+      delay(1000);
+    }
+
+    else if( sensormV3 <= 1000) {
+      insert10();
+      delay(1000);
+    }
+  
+  
+    key = myKeypad.getKey();
+    if(key == '*') {
+      u8g.firstPage();
+      do {
+        drawAstro0();
+      } while(u8g.nextPage());
+      delay(10);
+      input_key();
+      check_key();
+    }
+  
+    if(ServoSignal == 1) {
+      s.write(90);
+      set_up = 0;
+      ServoSignal = 0;
+      saving = 0;
+    }
+  }
+}
+
+void setGoal()
+{
+  int i;
+  int sum = 0;
+  for(i = 0; i < 5; i++) 
+  {
+    key = myKeypad.waitForKey();
+    if(key == '*') 
+    { 
+      break;
+      i--;
+    }
+    else 
+    {
+      goals[i] = (int)key;
+    }
   }
   
-  if(ServoSignal == 1) {
-    servo_act();
+  for(int j = i-1; j > -1; j--)
+  {
+    for(int k = 0; k < j; k++)
+    {
+      goals[k] *= 10;
+    }
   }
+  
+  for(int j = 0; j < i; j++)
+  {
+    sum = sum + goals[j];
+  }
+  goal = sum;
+  set_up = 1;
 }
 
 void password_key()   // 패스워드 설정 함수
 {     
-  for(int i = 0 ; i <4 ; i++)
+  for(int i = 0 ; i < 4 ; i++)
   {
     key = myKeypad.waitForKey();
     if(i == 0) {
@@ -224,18 +292,18 @@ void check_key() // 패스워드 비교 함수
   }
 }
 
-void servo_act() // 서보모터 동작 함수
+void servo_open() // 서보모터 동작 함수
 {    
   pinMode(10, OUTPUT);        // ( pin의 넘버, pin의 모드 ) 모드 => INPUT, OUTPUT, INPUT_PULLUP 
-  s.write(90);
+  s.write(90);                // 90 => 열림    180 => 닫힘
   delay(2000);
-//  key = myKeypad.waitForKey();
-//  if( key == '*') {
-//    s.write(90);
-//    delay(2000);
-//    s.write(0);
-//  }
+}
+
+void servo_close() // 서보모터 동작 함수
+{    
+  pinMode(10, OUTPUT);        // ( pin의 넘버, pin의 모드 ) 모드 => INPUT, OUTPUT, INPUT_PULLUP 
   s.write(180);
+  delay(2000);
   ServoSignal = 0;
 }
 
@@ -250,6 +318,31 @@ void drawMain()   // Main
   u8g.drawStr(80, 40, goalB);
   u8g.drawStr(65, 40, "/" );
   u8g.drawStr(0, 20, " Current Saving" );
+}
+
+//void drawGoal(int g[], int n) 
+//{    
+//  u8g.setFont(u8g_font_unifont);
+//  u8g.drawStr(20, 20, "Set GOALS");
+//  u8g.drawStr(10, 40, "Goal : ");
+//  for(int i = 0; i < n; i++) 
+//  {
+//    u8g.drawStr(30+10*i, 40, 
+//  }
+//}
+
+void drawSetG() 
+{    
+  u8g.setFont(u8g_font_unifont);
+  u8g.drawStr(20, 20, "Set GOALS!!");
+  u8g.drawStr(10, 40, "Goal : ");
+}
+
+void drawSetP() 
+{    
+  u8g.setFont(u8g_font_unifont);
+  u8g.drawStr(20, 20, "Set Password");
+  u8g.drawStr(10, 40, "PW : ");
 }
 
 void drawAstro0() 
